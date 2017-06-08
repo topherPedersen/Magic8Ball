ee6d19a54928f312050b76ab2736c35994dae243
crispytx <chrispedatx@gmail.com>
2017-06-08 00:42:41 -0500

    wired up second activity

--- app/src/main/java/xyz/personalhomepage/magic8ball/MainActivity.java
+++ app/src/main/java/xyz/personalhomepage/magic8ball/MainActivity.java
@@ -1,13 +1,26 @@
 package xyz.personalhomepage.magic8ball;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "xyz.personalhomepage.magic8ball.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Called when the user presses the "Submit" button
+    public void askQuestion(View view) {
+        // do stuff
+        Intent intent = new Intent(this, AnswerActivity.class);
+        EditText questionEditText = (EditText) findViewById(R.id.questionEditText);
+        String message = questionEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
